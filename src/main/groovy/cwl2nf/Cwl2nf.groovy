f8ac4abf48d8cefaf882ac07513cf7e6ac3401db
Kevin Sayers <kevin.sayers@crg.eu>
2017-03-22 11:08:09 +0100

    First commit for CWL to Nextflow converter

---
+++ src/main/groovy/cwl2nf/Cwl2nf.groovy
@@ -0,0 +1,76 @@
+#!/usr/bin/env groovy
+package cwl2nf
+
+/*
+@Grab(group='org.yaml', module='snakeyaml', version='1.18')
+*/
+
+import org.yaml.snakeyaml.Yaml
+
+
+class Cwl2nf{
+    static void main(String[] args) {
+		String workingDir = System.getProperty("user.dir")
+		String infile = ''
+		String ymlfile = ''
+		String inpath = args[0]
+		String ymlpath = args[1]
+		Yaml parser = new Yaml()
+		infile = new File(inpath).text
+		ymlfile = new File(ymlpath).text
+
+		def data = parser.load(infile)
+		def ymldata = parser.load(ymlfile)
+
+
+
+		def stepList = []
+		Map ymlmapping = [:]
+		def channelList = []
+		Map wfinputs = [:]
+
+
+		ymldata.keySet().each{
+			if (ymldata[it].getClass() == String){
+				ymlmapping.put(it, ymldata[it])
+				channelList.add(new String(it + ' = Channel.from("' + ymldata[it]) + '")')
+			}
+			if (ymldata[it].getClass() == LinkedHashMap){
+				ymlmapping.put(it, ymldata[it]['path'])
+				channelList.add(new String(it + ' = Channel.fromPath("' +ymldata[it]['path'] + '")' ))
+			}
+
+		}
+
+
+		data['steps'].keySet().each{
+			def stepins = data['steps'][it]['in']
+			def stepFilename = data['steps'][it]['run']
+			def stepID = stepFilename.replace('.cwl','')
+			stepFilename = inpath.replace(inpath.split('/')[-1], stepFilename)
+
+			if(stepID.contains('-')){
+				stepID = stepID.replace('-','_')
+			}
+			def stepFile = new File(stepFilename).text
+			def stepcwl = parser.load(stepFile)
+			stepList.add(new Step(stepcwl, stepID, data, stepins, ymlmapping))
+		}
+
+
+		String fileName = new Date().getTime() + '.nf'
+		def outfile = new File(fileName)
+		channelList.each{
+			println(it)
+			outfile.append(it + '\n')
+		}
+		stepList.each{
+			println(it.get_process_block())
+			outfile.append(it.get_process_block() + '\n')
+		}
+
+		//println(new String("nextflow run " + fileName).execute().text)
+
+    }
+}
+
---
+++ src/main/groovy/cwl2nf/Step.groovy
@@ -0,0 +1,195 @@
+#!/usr/bin/env groovy
+package cwl2nf 
+
+class Step{
+	String cmd_string
+	String id
+	def inputs
+	def outputs
+	def wfouts //These are the final files which are kept
+
+
+	Step(stepdata, id, wfdata, stepins, ymldata){
+		this.cmd_string = extract_command_string(stepdata)
+		this.id = id
+		this.inputs = extract_inputs(stepdata, wfdata, stepins)
+		this.outputs = extract_outputs(stepdata,wfdata, stepins, ymldata)
+		this.wfouts = extract_wfouts(wfdata)
+
+
+	}
+	def extract_command_string(cwldata){
+		def counter = 0 
+		def cmdstr = ''
+		//Deal with if the base command is a string or a list of commands
+		if (cwldata['baseCommand'].getClass() == String){
+			cmdstr += cwldata['baseCommand']
+		}
+		else{
+			cmdstr += cwldata['baseCommand'].join(" ")
+
+		}
+		cmdstr = cmdstr + extract_arguments(cwldata)
+
+		cwldata['inputs'].keySet().each{
+			if('prefix' in cwldata['inputs'][it]['inputBinding']){
+				cmdstr = cmdstr + ' ' + cwldata['inputs'][it]['inputBinding']['prefix']
+			}
+			cmdstr = cmdstr + ' ${invar_' + counter + '}'
+			counter += 1
+
+		}
+	
+		return cmdstr
+
+	}
+	def extract_arguments(cwldata){
+		Map cwl_vals = ['$(runtime.outdir)':'./']
+		def tmplist = ['',]
+		if ('arguments' in cwldata.keySet()){
+			cwldata['arguments'].each{
+				if(cwl_vals.containsKey(it)){
+					tmplist.add(cwl_vals[it])
+				}
+				else{
+					tmplist.add(it)
+				}
+			}
+
+			return tmplist.join(" ")
+		}
+
+		else{
+			return ''
+		}
+	}
+	def extract_inputs(cwldata, wfdata, stepins){
+		def typemap = ['File':'file', 'string':'val']
+		def inputsreturn = []
+		int counter = 0
+
+		cwldata['inputs'].keySet().each{
+			def intype = typemap[cwldata['inputs'][it]['type']]
+			def from = stepins[it]
+			if(from.getClass() == String){
+				if(from.contains('/')){
+					from = from.split('/')[-1]
+				}
+			}
+			if(from.getClass() == LinkedHashMap){
+				from = from[from.keySet()[0]]
+			}
+
+			inputsreturn.add(new String(intype + ' invar_' + counter + ' from ' + from))
+			counter += 1
+
+		}
+		return inputsreturn
+
+	}
+	def extract_outputs(cwldata, wfdata, stepins, ymldata){
+		def typemap = ['File':'file', 'string':'val']
+		def outputs = []
+		def glob = ''
+		def outType = ''
+/*		println(cwldata.keySet())
+*/		
+
+		if(cwldata.keySet().contains('stdout')){
+			if(cwldata['stdout'].contains('.')){
+				typemap.put('stdout','file')
+			}
+		}
+
+		cwldata['outputs'].keySet().each{
+			outType = typemap[cwldata['outputs'][it]['type']]
+			def into = it
+			def keycheck = cwldata['outputs'][it].keySet()
+			if(keycheck.contains('outputBinding')){
+				glob =cwldata['outputs'][it]['outputBinding']['glob']
+				if (glob.contains('$(inputs')){
+					glob = glob.replace("\$(inputs.",'')
+					glob = glob.replace(")",'')
+					glob = ymldata[stepins[glob]]
+				}
+				outputs.add(new String(outType + ' "' + glob + '" into ' + into ))
+
+			}
+			if(keycheck.size() == 1){
+				outType = typemap[cwldata['outputs'][it]['type']]
+				glob = cwldata['stdout']
+				outputs.add(new String(outType + ' "' + glob + '" into ' + into ))
+
+				//This covers a specific case where no file is output as named
+				//normally this would cause nextflow to crash.
+				if(this.cmd_string.contains('gunzip -c')){
+					if(!this.cmd_string.contains('>')){
+						this.cmd_string += ' > ' + glob
+					}
+				}
+			}
+
+
+		}
+
+		return outputs
+	}
+	def extract_wfouts(cwldata){
+		//efficiency of this step could be improved it currently parses
+		//the same thing for each step. 
+		def outs = []
+
+		cwldata['outputs'].keySet().each{
+			String outsrc = cwldata['outputs'][it]['outputSource']
+			if(outsrc.contains('/')){
+				outs.add(outsrc.split('/')[-1])
+			}
+		}
+
+		return outs
+	}
+	def get_cmd_string(){
+		return this.cmd_string
+	}
+	def get_id(){
+		return this.id
+	}
+	def get_inputs(){
+		return this.inputs
+	}
+	def get_outputs(){
+		return this.outputs
+	}
+	def get_process_block(){
+		String processString = ''
+		processString += '\n'
+		processString += 'process ' + this.id + '{ \n'
+		if(this.outputs.size() > 0){
+			this.outputs.each{
+				if(this.wfouts.contains(it.split(' ')[-1])){
+					processString += "\tpublishDir './', mode:'copy' \n"
+
+				}
+			}
+		}
+
+
+		processString += '\tinput: \n'
+		this.inputs.each{
+			processString += '\t' + it + '\n'
+		}
+		processString += '\toutput: \n'
+		this.outputs.each{
+			processString += '\t' + it + '\n'
+		}
+
+		processString += '\t"""\n'
+		processString += '\t' + this.cmd_string + '\n'
+		processString += '\t"""\n'
+		processString += '}'
+
+
+
+		return processString
+	}
+}
