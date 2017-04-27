0b8b41b5874c62c30186cb45917b1854db958ff6
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2017-04-27 10:56:54 +0200

    Added launcher script

---
+++ launch.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+#
+# Copyright (c) 2013-2016, Centre for Genomic Regulation (CRG).
+# Copyright (c) 2013-2016, Paolo Di Tommaso and the respective authors.
+#
+#   This file is part of 'Nextflow'.
+#
+#   Nextflow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Nextflow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Nextflow.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#set -e
+#set -u
+#set -o errexit
+
+# the application 'base' folder
+bin_dir=`dirname "$0"`
+bin_dir=`cd "$bin_dir"; pwd`
+base_dir=$bin_dir
+
+# define the java env
+java=java
+if test -n "$JAVA_HOME"; then
+	java="$JAVA_HOME/bin/java"
+fi
+
+#
+# debugging JVM options
+#  export _JAVA_OPTIONS='-Xms1g -Xmx2g -verbose:gc -XX:+AggressiveOpts -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions -XX:+PrintFlagsFinal -XX:+TraceClassLoading -XX:+TraceClassUnloading -verbose:class'
+#
+
+#
+# Variable definition
+#
+declare -a args=()
+DEBUG=''
+COLUMNS=${COLUMNS:-`tput cols 2> /dev/tty`}
+MAIN_CLASS=${MAIN_CLASS:-'cwl2nxf.Cwl2nxf'}
+JVM_ARGS+=" -Dfile.encoding=UTF-8 -noverify -XX:+TieredCompilation -XX:TieredStopAtLevel=1"
+## flight recorded -- http://docs.oracle.com/javacomponents/jmc-5-4/jfr-runtime-guide/run.htm
+##JVM_ARGS+=" -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:StartFlightRecording=duration=60s,filename=myrecording.jfr"
+NXF_OPTS=${NXF_OPTS:-}
+NXF_CLI="$0 $@"
+export NXF_CLI
+export COLUMNS
+
+#
+# classpath when the application is compiled with gradle
+#
+if [ -e $base_dir/build/libs/cwl2nxf-*.jar ]; then
+  CLASSPATH=`ls $base_dir/build/libs/cwl2nxf-*.jar`
+
+  # -- append runtime libraries
+  [[ ! -f "$base_dir/.launch.classpath" ]] && echo "Missing '.launch.classpath' file -- Create it by running: ./gradlew jar" && exit 1
+  CLASSPATH+=":`cat $base_dir/.launch.classpath`"
+  
+else
+  echo "Missing application libraries -- Create it by running: ./gradlew jar"
+  exit 1
+fi
+
+
+#
+# Handle special program cli options
+#
+while [ "$*" != "" ]; do
+  if [[ "$1" == '--debug' || "$1" == '--trace' ]]; then
+    args+=("$1")
+
+  elif [ "$1" == --with-yourkit ]; then 
+    JVM_ARGS+=" -agentpath:/Applications/YourKit_Java_Profiler_2014_build_14104.app/bin/mac/libyjpagent.jnilib=onexit=snapshot,tracing,dir=$PWD/snapshot "
+  elif [ "$1" == '--with-jrebel' ]; then
+    if [ "$JREBEL_HOME" ]; then
+    JVM_ARGS+=" -javaagent:$JREBEL_HOME/jrebel.jar -Drebel.log.file=./jrebel-client.log"
+    else
+    echo "WARN: To use JRebel define the JREBEL_HOME variable in environment"
+    fi
+
+  elif [ "$1" == '-remote-debug' ]; then
+    DEBUG='-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005'
+
+  else
+   args+=("$1")
+  fi
+  # move to the next option
+  shift
+done
+
+# Show some variable when in DEBUG mode
+if [ "$DEBUG" != '' ]; then
+  echo Launch environment
+  echo ------------------
+  echo base_dir: $base_dir
+  echo jvmargs: $JVM_ARGS
+  echo debug: $DEBUG
+  echo classpath:
+  echo $CLASSPATH | tr ":" "\n" | sort
+  echo ''
+  echo Launching it!
+  echo ------------------
+fi
+
+# Launch the APP
+exec java $JVM_ARGS $DEBUG $NXF_OPTS -cp "$CLASSPATH" "$MAIN_CLASS" "${args[@]}"
