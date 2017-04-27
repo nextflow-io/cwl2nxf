ebd422d90d466b7774addf039c50127a8e63d29c
Kevin Sayers <kevin.sayers@crg.eu>
2017-04-27 14:10:53 +0200

    Added test for arguments

--- src/test/groovy/cwl2nxf/StepTest.groovy
+++ src/test/groovy/cwl2nxf/StepTest.groovy
@@ -9,7 +9,6 @@ import spock.lang.Specification
  */
 class StepTest extends Specification {
 
-
     def 'should extract the command string' () {
 
         given:
@@ -38,6 +37,33 @@ class StepTest extends Specification {
         then:
         cmd == 'bowtie2-build ${invar_0} ${invar_1}'
 
+    }
+    def 'should extract arguments' (){
+        given:
+        def text = '''
+        cwlVersion: v1.0
+        class: CommandLineTool
+        baseCommand: bowtie2-build
+        arguments: ["-d", $(runtime.outdir)]
+        
+        inputs:
+          indexfile:
+            type: File
+            inputBinding:
+              position: 1
+          doing:
+            type: string
+            inputBinding:
+              position: 2
+        '''.stripIndent()
+
+        def cwl = (Map)new Yaml().load(text)
+        def step = new Step()
+
+        when:
+        def argsreturn = step.extractArguments(cwl)
+        then:
+        argsreturn == ' -d ./'
     }
 
 }
